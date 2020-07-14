0f085386347b9ca60b52b7d0e895de8018d2b361
Christoph Hellwig <hch@lst.de>
2020-07-14 08:55:05 +0200

    fs: add a vfs_fchmod helper
    [ Upstream commit 9e96c8c0e94eea2f69a9705f5d0f51928ea26c17 ]
    
    Add a helper for struct file based chmode operations.  To be used by
    the initramfs code soon.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Stable-dep-of: 4624b346cf67 ("init: open /initrd.image with O_LARGEFILE")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1734,6 +1734,7 @@ int vfs_fchown(struct file *file, uid_t user, gid_t group);
 int vfs_fchmod(struct file *file, umode_t mode);
 
 int vfs_fchown(struct file *file, uid_t user, gid_t group);
+int vfs_fchmod(struct file *file, umode_t mode);
 
 extern long vfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
