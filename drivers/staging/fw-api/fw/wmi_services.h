b931ab44583bfb1e968650f7720b18fca0fe55ec
spuligil <quic_spuligil@quicinc.com>
2024-02-10 01:16:33 -0800

    fw-api: CL 25914677 - update fw common interface files
    WMI: change QMS_DLKM to SMEM_MAILBOX
    Change-Id: Ia377773337031334e3b678db28b148b291246f69
    CRs-Fixed: 2262693

--- drivers/staging/fw-api/fw/wmi_services.h
+++ drivers/staging/fw-api/fw/wmi_services.h
@@ -652,6 +652,11 @@ typedef  enum  {
     WMI_SERVICE_VDEV_DCS_STATS_SUPPORT = 399, /* FW supports VDEV level DCS stats reporting */
     WMI_SERVICE_WIFI_RADAR_SUPPORT = 400, /* FW supports wifi radar */
     WMI_SERVICE_P2P_DEVICE_UPDATE_MAC_ADDR_SUPPORT = 401, /* indicate FW support dynamic update mac address for P2P device */
+    WMI_SERVICE_DCS_OBSS_INT_SUPPORT = 402, /* Indicates FW supports OBSS Interference detection */
+    WMI_SERVICE_DYNAMIC_WSI_REMAP_SUPPORT = 403, /* WSI bypass remap is supported by Firmware */
+    WMI_SERVICE_QMS_DLKM_SUPPORT = 404, /* DEPRECATED */
+    WMI_SERVICE_SMEM_MAILBOX_SUPPORT = 404, /* FW code has smem_mailbox support enabled */
+    WMI_SERVICE_REG_CC_EXT2_EVENT_SUPPORT = 405, /* Indicate FW would send EXT2 REG_CC event having data which would be a continuation to EXT REG_CC event */
 
 
     WMI_MAX_EXT2_SERVICE
