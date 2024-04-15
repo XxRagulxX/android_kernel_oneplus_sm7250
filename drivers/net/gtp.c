b7f98e67459c55dc996e4fef6928ae7b6e9d21da
Greg Kroah-Hartman <gregkh@google.com>
2024-04-15 15:27:30 +0000

    Merge 4.19.308 into android-4.19-stable
    Changes in 4.19.308
    	net/sched: Retire CBQ qdisc
    	net/sched: Retire ATM qdisc
    	net/sched: Retire dsmark qdisc
    	stmmac: no need to check return value of debugfs_create functions
    	net: stmmac: fix notifier registration
    	memcg: add refcnt for pcpu stock to avoid UAF problem in drain_all_stock()
    	nilfs2: replace WARN_ONs for invalid DAT metadata block requests
    	userfaultfd: fix mmap_changing checking in mfill_atomic_hugetlb
    	sched/rt: Fix sysctl_sched_rr_timeslice intial value
    	sched/rt: sysctl_sched_rr_timeslice show default timeslice after reset
    	sched/rt: Disallow writing invalid values to sched_rt_period_us
    	scsi: target: core: Add TMF to tmr_list handling
    	dmaengine: shdma: increase size of 'dev_id'
    	wifi: cfg80211: fix missing interfaces when dumping
    	wifi: mac80211: fix race condition on enabling fast-xmit
    	fbdev: savage: Error out if pixclock equals zero
    	fbdev: sis: Error out if pixclock equals zero
    	ahci: asm1166: correct count of reported ports
    	ext4: avoid allocating blocks from corrupted group in ext4_mb_try_best_found()
    	ext4: avoid allocating blocks from corrupted group in ext4_mb_find_by_goal()
    	regulator: pwm-regulator: Add validity checks in continuous .get_voltage
    	hwmon: (coretemp) Enlarge per package core count limit
    	firewire: core: send bus reset promptly on gap count error
    	virtio-blk: Ensure no requests in virtqueues before deleting vqs.
    	s390/qeth: Fix potential loss of L3-IP@ in case of network issues
    	pmdomain: renesas: r8a77980-sysc: CR7 must be always on
    	IB/hfi1: Fix sdma.h tx->num_descs off-by-one error
    	mm: memcontrol: switch to rcu protection in drain_all_stock()
    	dm-crypt: don't modify the data when using authenticated encryption
    	gtp: fix use-after-free and null-ptr-deref in gtp_genl_dump_pdp()
    	l2tp: pass correct message length to ip6_append_data
    	ARM: ep93xx: Add terminator to gpiod_lookup_table
    	usb: gadget: ncm: Avoid dropping datagrams of properly parsed NTBs
    	usb: roles: don't get/set_role() when usb_role_switch is unregistered
    	IB/hfi1: Fix a memleak in init_credit_return
    	RDMA/bnxt_re: Return error for SRQ resize
    	RDMA/srpt: Support specifying the srpt_service_guid parameter
    	RDMA/ulp: Use dev_name instead of ibdev->name
    	RDMA/srpt: Make debug output more detailed
    	RDMA/srpt: fix function pointer cast warnings
    	scripts/bpf: teach bpf_helpers_doc.py to dump BPF helper definitions
    	bpf, scripts: Correct GPL license name
    	scsi: jazz_esp: Only build if SCSI core is builtin
    	nouveau: fix function cast warnings
    	ipv6: sr: fix possible use-after-free and null-ptr-deref
    	packet: move from strlcpy with unused retval to strscpy
    	s390: use the correct count for __iowrite64_copy()
    	PCI/MSI: Prevent MSI hardware interrupt number truncation
    	KVM: arm64: vgic-its: Test for valid IRQ in its_sync_lpi_pending_table()
    	KVM: arm64: vgic-its: Test for valid IRQ in MOVALL handler
    	fs/aio: Restrict kiocb_set_cancel_fn() to I/O submitted via libaio
    	scripts/bpf: Fix xdp_md forward declaration typo
    	Linux 4.19.308
    
    Change-Id: I5d0daaa03dbc35f1460154d9b04fc4c625205974
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/net/gtp.c
+++ drivers/net/gtp.c
@@ -1385,18 +1385,20 @@ static int __init gtp_init(void)
 	if (err < 0)
 		goto error_out;
 
-	err = rtnl_link_register(&gtp_link_ops);
+	err = register_pernet_subsys(&gtp_net_ops);
 	if (err < 0)
-		goto unreg_pernet_subsys;
+		goto unreg_rtnl_link;
 
 	err = genl_register_family(&gtp_genl_family);
 	if (err < 0)
-		goto unreg_rtnl_link;
+		goto unreg_pernet_subsys;
 
 	pr_info("GTP module loaded (pdp ctx size %zd bytes)\n",
 		sizeof(struct pdp_ctx));
 	return 0;
 
+unreg_pernet_subsys:
+	unregister_pernet_subsys(&gtp_net_ops);
 unreg_rtnl_link:
 	rtnl_link_unregister(&gtp_link_ops);
 unreg_pernet_subsys:
