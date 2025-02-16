77adaf8b816a9e120f4b7042237fd3f35bcc9014
XxRagulxX <donragulvijay@gmail.com>
2025-02-16 10:15:23 +0530

    Custom kernel modification: Added My Name

--- init/version.c
+++ init/version.c
@@ -44,7 +44,8 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION 
+	" - Modified by King\n";
 
 const char *linux_banner_ptr = linux_banner;
 EXPORT_SYMBOL_GPL(linux_banner_ptr);
