5cade4a999ba5c548f890bff2066aa9712256a76
XxRagulxX <donragulvijay@gmail.com>
2025-03-24 11:07:26 +0530

    Revert "rpmsg: glink: Propagate TX failures in intentless mode as well"
    This reverts commit 04c2c00fcc07625b5feceaf3b3a3aeb8b2cd061a.

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1642,9 +1642,8 @@ static int __qcom_glink_send(struct glink_channel *channel,
 	ret = qcom_glink_tx(glink, &req, sizeof(req), data, chunk_size, wait);
 
 	/* Mark intent available if we failed */
-	if (ret) {
-		if (intent)
-			intent->in_use = false;
+	if (ret && intent) {
+		intent->in_use = false;
 		return ret;
 	}
 
@@ -1665,9 +1664,8 @@ static int __qcom_glink_send(struct glink_channel *channel,
 				    chunk_size, wait);
 
 		/* Mark intent available if we failed */
-		if (ret) {
-			if (intent)
-				intent->in_use = false;
+		if (ret && intent) {
+			intent->in_use = false;
 			break;
 		}
 	}
